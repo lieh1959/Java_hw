5d42e543d57e6dda2860f0ac68455273a7e590dd
lieh1959 <lieh1959@gmail.com>
2021-09-27 11:58:01 +0800

    Add files via upload

---
+++ Test/Class02.java
@@ -0,0 +1,14 @@
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbbb obj1=new Cbbb();		//(a)
+		Cbbb obj2=new Cbbb();
+		Cbbb avg=new Cbbb();
+		obj1.x=5.2;  obj1.y=3.9;   // (b)
+		obj2.x=6.5;  obj2.y=4.6;   // (b)
+		
+	}
+
+}
