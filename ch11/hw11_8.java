4f6ae29e14a32192897259bf64caf2c8fab5a299
lieh1959 <lieh1959@gmail.com>
2021-12-12 22:55:38 +0800

    Add files via upload

---
+++ ch11/hw11_8.java
@@ -0,0 +1,37 @@
+// hw11_8,
+interface iAaa
+{
+   public void show();
+}
+
+class Cbbb
+{
+   public int num=10;
+
+   public void set(int n)
+   {
+      num=n;
+   }
+}
+
+class Cccc extends Cbbb implements iAaa
+{
+   public void show()
+   {
+      System.out.println("num="+num);
+   }
+}
+
+public class hw11_8
+{
+   public static void main(String args[])
+   {
+      Cccc cc=new Cccc();
+      cc.set(5);
+      cc.show();    // ¦L¥Xnum=5
+   }
+}
+
+/* output----
+num=5
+-----------*/
